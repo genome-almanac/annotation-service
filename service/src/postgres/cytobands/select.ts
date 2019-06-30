7e5510a674081cd3c1241dd6a1c90027759d334e
hpratt <hpratt111@live.com>
2019-06-29 23:34:55 -0400

    fix injection

--- service/src/postgres/cytobands/select.ts
+++ service/src/postgres/cytobands/select.ts
@@ -9,10 +9,10 @@ SELECT chromosome, startcoordinate AS start, endcoordinate AS stop, bandname, st
 export async function selectCytobands(assembly: string, parameters: CytobandParameters,
 				      db: IDatabase<any>): Promise<CytobandResult[]> {
     const tableName = "cytobands_" + assembly;
-    const whereclause: string = ' ' + whereClause(cytobandConditions(parameters, tableName));
+    const whereclause: string = ' ' + whereClause(cytobandConditions(parameters, "cytoband_table"));
     const orderBy = " ORDER BY chromosome, bandname";
     const limit: string = parameters.limit ? ` LIMIT ${parameters.limit}` : "";
     const offset: string = parameters.offset ? ` OFFSET ${parameters.offset}` : "";
-    return db.any(SELECT + ` FROM ${tableName} WHERE ` + whereclause + orderBy + limit + offset,
-		  { [tableName]: parameters });
+    return db.any(SELECT + " FROM ${tableName~} AS cytoband_table WHERE " + whereclause + orderBy + limit + offset,
+		  { cytoband_table: parameters, tableName });
 };
