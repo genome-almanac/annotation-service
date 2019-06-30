a80f0af3389b5a8f04245565e5b571460a0a3246
hpratt <hpratt111@live.com>
2019-06-29 22:04:21 -0400

    deployment configuration and scripts

---
+++ scripts/lib/common.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+GCR_PROJECT_ID=devenv-215523
+SERVICE_DOCKER_IMAGE_NAME=annotations-service
+IMPORTER_DOCKER_IMAGE_NAME=annotations-importer
+KUBE_DEPLOYMENT_NAME=annotations-service-deployment
+KUBE_DEPLOYMENT_CONTAINER_NAME=annotations-service
